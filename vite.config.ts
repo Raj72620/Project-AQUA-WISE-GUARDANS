5076433b296f697e83b15ba113fecd32018fa922
Raj Nishanth <nishanthraj9618@gmail.com>
2025-08-05 13:32:29 +0530

    Updated vite config and HTML

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,22 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/Project-AQUA-WISE-GUARDANS/',
-  server: {
-    host: "::",
-    port: 8080,
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
   },
-  plugins: [
-    react()
-  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 });
